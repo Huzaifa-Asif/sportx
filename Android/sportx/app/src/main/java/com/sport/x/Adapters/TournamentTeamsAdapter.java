9cb4d0d4405c95390c8e92e33f1be12f11b26ff9
huzaifa85801 <huzaifa8580@gmail.com>
2019-09-08 23:21:05 +0500

    Tournament Teams management added

---
+++ Android/sportx/app/src/main/java/com/sport/x/Adapters/TournamentTeamsAdapter.java
@@ -0,0 +1,213 @@
+package com.sport.x.Adapters;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
+import com.sport.x.Misc.Misc;
+import com.sport.x.Models.Expense;
+import com.sport.x.Models.TournamentTeam;
+import com.sport.x.R;
+import com.sport.x.SharedPref.SharedPref;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+
+
+public class TournamentTeamsAdapter extends RecyclerView.Adapter<TournamentTeamsAdapter.TournamentTeamsViewHolder> {
+
+
+    private ArrayList<TournamentTeam> teams = new ArrayList<>();
+    private Context context;
+    Misc misc;
+    SharedPref sharedPref;
+    ImageButton details;
+    public TournamentTeamsAdapter(Context context, ArrayList<TournamentTeam> teams){
+        this.context = context;
+        this.teams = teams;
+        misc = new Misc(context);
+
+    }
+    @NonNull
+    @Override
+    public TournamentTeamsAdapter.TournamentTeamsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.item_team, viewGroup, false);
+        return new TournamentTeamsAdapter.TournamentTeamsViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull TournamentTeamsAdapter.TournamentTeamsViewHolder TournamentTeamsViewHolder, int i) {
+        TournamentTeamsViewHolder.setData(teams.get(i));
+
+    }
+    @Override
+    public int getItemCount() {
+        return teams.size();
+    }
+
+
+    public class TournamentTeamsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView teamName_, teamState_, teamContact_;
+        private String teamName, teamState;
+
+
+        public TournamentTeamsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            teamName_ = itemView.findViewById(R.id.com_name);
+            teamState_ = itemView.findViewById(R.id.com_state);
+            teamContact_ = itemView.findViewById(R.id.com_contact);
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void setData(final TournamentTeam team) {
+            final TournamentTeam teamDetails = team;
+            teamName_.setText(team.getTournamentTeamName());
+            teamState_.setText(team.getTournamentTeamState());
+            teamContact_.setText(team.getTournamentTeamContact());
+        }
+
+//        TournamentTeam team
+        public void callDeleteExpenseWebservice(boolean isShowDialog,String id) {
+
+//            final TournamentTeam deletedTeam=team;
+
+            Ion.with(context)
+                    .load("DELETE", misc.ROOT_PATH + "delete_expense/" + id)
+                    .asString()
+                    .withResponse()
+                    .setCallback(new FutureCallback<Response<String>>() {
+                        @Override
+                        public void onCompleted(Exception e, Response<String> result) {
+                            if (e != null) {
+
+                                misc.showToast("Please check your connection");
+
+                                return;
+                            }
+
+                            try {
+                                JSONObject jsonObjectExpenseDeleted = new JSONObject(result.getResult());
+                                Boolean status = jsonObjectExpenseDeleted.getBoolean("status");
+                                if(status) {
+//                                    teams.remove(deletedTeam);
+//                                    notifyDataSetChanged();
+                                }
+                            } catch (JSONException e1) {
+                                e1.printStackTrace();
+                            }
+
+
+                        }
+
+                    });
+        }
+
+        public void callUpdateStateWebservice(boolean isShowDialog,String id) {
+
+//            final TournamentTeam deletedTeam=team;
+
+            Ion.with(context)
+                    .load("DELETE", misc.ROOT_PATH + "delete_expense/" + id)
+                    .asString()
+                    .withResponse()
+                    .setCallback(new FutureCallback<Response<String>>() {
+                        @Override
+                        public void onCompleted(Exception e, Response<String> result) {
+                            if (e != null) {
+
+                                misc.showToast("Please check your connection");
+
+                                return;
+                            }
+
+                            try {
+                                JSONObject jsonObjectExpenseDeleted = new JSONObject(result.getResult());
+                                Boolean status = jsonObjectExpenseDeleted.getBoolean("status");
+                                if(status) {
+//                                    teams.remove(deletedTeam);
+//                                    notifyDataSetChanged();
+                                }
+                            } catch (JSONException e1) {
+                                e1.printStackTrace();
+                            }
+
+
+                        }
+
+                    });
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            Log.d("adapter position: ",""+getAdapterPosition());
+
+                    final Dialog dialog = new Dialog(context);
+                    dialog.setContentView(R.layout.team_details_popup);
+                    TextView teamName = dialog.findViewById(R.id.teamName);
+                    teamName.setText(teams.get(getAdapterPosition()).getTournamentTeamName());
+                    final TextView teamContact = dialog.findViewById(R.id.teamContact);
+                    teamContact.setText(teams.get(getAdapterPosition()).getTournamentTeamContact());
+                    TextView teamState = dialog.findViewById(R.id.teamState);
+                    teamState.setText(teams.get(getAdapterPosition()).getTournamentTeamState());
+
+                    teamContact.setOnClickListener(new View.OnClickListener()
+                    {
+                        public void onClick(View v)
+                        {
+                        Intent intent = new Intent(Intent.ACTION_CALL);
+                        intent.setData(Uri.parse("tel:" + teams.get(getAdapterPosition()).getTournamentTeamContact()));
+                        context.startActivity(intent);
+                        }
+                    });
+
+                    Button delete=dialog.findViewById(R.id.deleteTeam);
+                    delete.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v)
+                        {
+
+                            callDeleteExpenseWebservice(true,teams.get(getAdapterPosition()).getTournamentTeamId());
+                            dialog.dismiss();
+
+                        }
+                    });
+
+
+                    Button state=dialog.findViewById(R.id.updateState);
+                    state.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v)
+                        {
+                        callUpdateStateWebservice(true,teams.get(getAdapterPosition()).getTournamentTeamId());
+                        dialog.dismiss();
+
+                        }
+                    });
+                    dialog.show();
+        }
+
+    }
+
+    }
+
+
+
--- Android/sportx/app/src/main/java/com/sport/x/TournamentDetailActivity.java
+++ Android/sportx/app/src/main/java/com/sport/x/TournamentDetailActivity.java
@@ -1,6 +1,7 @@
 package com.sport.x;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -8,6 +9,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,20 +25,37 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
+import com.sport.x.Adapters.ExpenseAdapter;
+import com.sport.x.Adapters.TournamentTeamsAdapter;
+
 import com.sport.x.Misc.Misc;
+import com.sport.x.Models.Expense;
+import com.sport.x.Models.TournamentTeam;
 import com.sport.x.ServiceProviderActivities.ServiceHomeActivity;
 import com.sport.x.SharedPref.SharedPref;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class TournamentDetailActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    private TextView _name,_service_provider, _type, _no_of_teams, _entry_fee, _no_of_days,_start_date, _winning_prize;
+    private TextView _name,_service_provider, _type, _no_of_teams, _entry_fee, _no_of_days,_start_date, _winning_prize, _team_text;
     private String tournament_id,state, name,service_provider, type, no_of_teams, entry_fee, no_of_days,start_date,start_time, winning_prize;
     private Button add_team, accept_tournament, cancel_tournament;
 
     SharedPref sharedPref;
     Misc misc;
     private boolean show = false;
+    ArrayList<TournamentTeam> teams = new ArrayList<TournamentTeam>();
+    private RecyclerView teamRecycler;
+    private TournamentTeamsAdapter teamAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +75,7 @@ public class TournamentDetailActivity extends AppCompatActivity implements View.
         _no_of_days = findViewById(R.id.c_days);
         _start_date = findViewById(R.id.c_start_date);
         _winning_prize = findViewById(R.id.c_winning_prize);
-
+        _team_text = findViewById(R.id.no_team_text);
 
         add_team = findViewById(R.id.add_team);
         add_team.setOnClickListener(this);
@@ -81,9 +102,6 @@ public class TournamentDetailActivity extends AppCompatActivity implements View.
         service_provider = intent.getStringExtra("serviceProviderEmail");
 
 
-
-
-
         _name.setText("Name: " + name);
         _service_provider.setText("Service Provider: " + service_provider);
         _type.setText("Type: " + type);
@@ -93,7 +111,15 @@ public class TournamentDetailActivity extends AppCompatActivity implements View.
         _start_date.setText("Start Time: " + start_date + " "+ start_time);
         _winning_prize.setText("Prize: " + winning_prize);
 
+        teamRecycler =  findViewById(R.id.reyclerview_team_list);
 
+        teamAdapter = new TournamentTeamsAdapter(this, teams);
+        teamRecycler.setLayoutManager(new LinearLayoutManager(this));
+        teamRecycler.setAdapter(teamAdapter);
+
+
+        // method call to fetch teams
+        callTeamWebservice(true);
 
     }
 
@@ -138,6 +164,72 @@ public class TournamentDetailActivity extends AppCompatActivity implements View.
     public void cancelTournament()
     {
 
+    }
+
+
+
+    public void callTeamWebservice(boolean isShowDialog)
+    {
+
+        if(teams.size()==0) {
+
+        }
+        final int teamSize = teams.size();
+
+        Ion.with(this)
+                .load("GET", misc.ROOT_PATH + "get_team_by_tournament/" + tournament_id)
+                .asString()
+                .withResponse()
+                .setCallback(new FutureCallback<Response<String>>() {
+                    @Override
+                    public void onCompleted(Exception e, Response<String> result) {
+                        if (e != null) {
+
+                            misc.showToast("Please check your connection");
+
+                            return;
+                        }
+
+                        try {
+
+                            JSONArray jsonArray = new JSONArray(result.getResult());
+                            if (jsonArray.length() < 1) {
+                                _team_text.setVisibility(View.VISIBLE);
+//                                misc.showToast("No teams Found");
+                                return;
+                            }
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+
+                                JSONObject jsonObjectTeam = (JSONObject) jsonArray.get(i);
+                                String teamId = jsonObjectTeam.getString("_id");
+                                String tournamentId = jsonObjectTeam.getString("tournamentId");
+                                String name = jsonObjectTeam.getString("name");
+                                String state = jsonObjectTeam.getString("state");
+                                String teamContact = jsonObjectTeam.getString("teamContact");
+                                String adderEmail = jsonObjectTeam.getString("adderEmail");
+//                              String players = jsonObjectTeam.getString("name");
+                                JSONArray players = new JSONArray(jsonObjectTeam.getString("players"));
+
+//
+
+//                                Log.d( "TeamPlayers: ",TeamPlayers.getString(0));
+
+                                teams.add(new TournamentTeam(state, name, players, tournamentId, teamId, adderEmail, teamContact));
+
+                            }
+
+                            teamAdapter.notifyDataSetChanged();
+
+
+                        } catch (JSONException e1) {
+                            e1.printStackTrace();
+                        }
+//
+
+                    }
+
+                });
     }
 
 
