b1c835c80ff491fb87b50849187fbb1c9e4d6fd5
huzaifa85801 <huzaifa8580@gmail.com>
2019-11-26 13:41:19 +0500

    Tournament Completed

---
+++ Android/app/src/main/java/com/sport/x/Activities/CustomerActivities/TournamentActivity.java
@@ -0,0 +1,105 @@
+package com.sport.x.Activities.CustomerActivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.sport.x.Activities.Menu.Menu;
+import com.sport.x.Fragments.CustomerTournamentCompleted;
+import com.sport.x.Fragments.CustomerTournamentInProgress;
+import com.sport.x.R;
+import com.sport.x.SharedPref.SharedPref;
+
+public class TournamentActivity extends Menu {
+
+    /**
+     * The {@link PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+    SharedPref sharedPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.inflateView(R.layout.activity_c_tournament);
+        setTitle("Tournaments");
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setTitle("Booking Management");
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        sharedPref = new SharedPref(this);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+    }
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    CustomerTournamentInProgress customerTournamentInProgress = new CustomerTournamentInProgress();
+                    return customerTournamentInProgress;
+                case 1:
+                    CustomerTournamentCompleted customerTournamentCompleted = new CustomerTournamentCompleted();
+                    return customerTournamentCompleted;
+                default:
+                    return null;
+
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+            Intent intent = new Intent(this, com.sport.x.Activities.CustomerActivities.HomeActivity.class);
+            startActivity(intent);
+            finish();
+
+    }
+}
